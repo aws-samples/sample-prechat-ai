045329d3c2a3184fe6c0a514eb4bd7b930151e26
binchoo <jbinchoo@gmail.com>
2025-10-13 13:42:34 +0900

    feat: Resolve AWS SAM IAM capabilities issue

--- deploy-full.sh
+++ deploy-full.sh
@@ -33,6 +33,8 @@ sam deploy \
   --profile $PROFILE \
   --region $REGION \
   --stack-name $STACK_NAME \
+  --resolve-s3 \
+  --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
   --parameter-overrides "Stage=\"$STAGE\" BedrockRegion=\"$BEDROCK_REGION\""
 
 # Step 4: Update environment variables
